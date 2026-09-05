9239fbc518fd8334d89acd0ba46627414019418e
hp <alisabetdeveloper@gmail.com>
2026-09-05 17:05:47 +0330

    handled some cases

--- search.py
+++ search.py
@@ -5,12 +5,16 @@ p = Path("/home/hp/dir1")
 
 def dfs_search(path, file_name):
     sub_dirs = []
-    for item in path.iterdir():
-        if item.is_file():
-            if item.name == file_name:
-                return item.absolute()
-        elif item.is_dir():
-            sub_dirs.append(item)
+    try:
+        for item in path.iterdir():
+            if not item.is_symlink():
+                if item.is_file():
+                    if item.name == file_name:
+                        return item.absolute()
+                elif item.is_dir():
+                    sub_dirs.append(item)
+    except:
+        print(f"permission denied: {path.absolute()}")
     for sub_dir in sub_dirs:
         result = dfs_search(sub_dir, file_name)
         if result is not None:
