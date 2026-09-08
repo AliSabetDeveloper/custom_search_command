e865f9484d2c7cd58b9da8245daf26a96ef4c7dc
hp <alisabetdeveloper@gmail.com>
2026-09-08 14:10:26 +0330

    added statistic to dfs

--- search.py
+++ search.py
@@ -3,24 +3,30 @@ p = Path("/home/hp/dir1")
 
 
 
-def dfs_search(path, file_name):
+def dfs_search(path, file_name, result=None):
+    if result is None:
+        result = {"file_name": None, "file_visited": 0, "directory_visited": 1}
     sub_dirs = []
     try:
+        print(path)
         for item in path.iterdir():
             if not item.is_symlink():
                 if item.is_file():
+                    result["file_visited"] += 1
                     if item.name == file_name:
-                        return item.absolute()
+                        result["file_name"] = str(item)
+                        return result
                 elif item.is_dir():
                     sub_dirs.append(item)
     except:
         print(f"permission denied: {path.absolute()}")
     for sub_dir in sub_dirs:
-        result = dfs_search(sub_dir, file_name)
-        if result is not None:
+        result["directory_visited"] += 1
+        result = dfs_search(sub_dir, file_name, result)
+        if result["file_name"] is not None:
             return result
-    return None
+    return result
 
 
 
-print(dfs_search(p, 'file1'))
+print(dfs_search(p, 'file8'))
